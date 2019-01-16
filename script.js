29e6a8045553b8797b9b0d156ce14c1d4f4ca044
Som Bohora <som-bohora@ouhsc.edu>
2019-01-15 18:20:54 -0600

    update files

--- script.js
+++ script.js
@@ -9,45 +9,59 @@ var toTitleCase = function (str) {
 const app = new Clarifai.App({ apiKey: 'c4226e96294a4238a5b4fa8ac9aae598' });
 const appModel = 'bd367be194cf45149e75f01d59f77ba7';
 
+const nutrientURL = 'http://api.wolframalpha.com/v2/query?input='
+const nutrientAPI = 'JLHA8Y-3WRLXGJQHY'
+
 const image = 'https://samples.clarifai.com/food.jpg';
 
-$(function() {
+function getPreditedData() {
 	app.models.predict(appModel, image)
     // .then(res => res.json(res))
     // .then(res => console.log(res))
     .then(out => {
-      let frames = out.outputs[0].data.concepts;
-      frames.forEach(frame => {
-        console.log(toTitleCase(frame.name), frame.value)
-      })
-    })
-    .catch(error => console.log(error));
-    
-  require('plotly')('energeticsom', '8gyeFQPQDjZyHSmogidW');
+      let manyItems = out.outputs[0].data.concepts;
+      let oneItem = out.outputs[0].data.concepts[0].name;
+      let foodItemURL = nutrientURL + oneItem + '%20nutrition%20facts&appid='+nutrientAPI;
+      console.log(foodItemURL)
 
-var data = [
-  {
-    x: ["giraffes", "orangutans", "monkeys"],
-    y: [20, 14, 23],
-    type: "bar"
-  }
-];
-var graphOptions = {filename: "basic-bar", fileopt: "overwrite"};
-plotly.plot(data, graphOptions, function (err, msg) {
-    console.log(msg);
-});
+        $('#concepts').html('<h3>'+ oneItem + '</h3>')
+        $('#myImg').html(`<img src='"${foodItemURL}"'>`);
 
-// // 	// fetch(
-// // 	// 	`https://samples.clarifai.com/food.jpg`
-// // 	// )
-// // 		.then(res => res.json())
-// // 		.then(json => console.log(json))
-// // 		.catch(err => console.log(err));
-// // 	// fetch('https://samples.clarifai.com/food.jpg')
-// // 	// 	.then(res => res.json())
-// // 	// 	.then(json => console.log(json.avatar_url))
-// // 	// 	.catch(err => console.log(err));
-});
+      // getNutritionalInfo(foodItemURL, function (result) {
+      //   $('#concepts').html('<h3>'+ oneItem + '</h3>') + "<img src='"+foodItemURL+"'>");
+      // });
+    }, function(err){console.log(err)})
+};
+
+
+// function makeTable() {
+//     getPreditedData();
+//     $('#tbl-food').append('<table><tr><th>Food</th><th>Probability</th></tr></table>');
+//     var myTable = $('#tbl-food').children();
+//     manyItems.forEach(frame => {
+//       myTable.append(`<tr><td>${frame.name}</td><td>${frame.value.toFixed(4)}</td></tr>`);
+//     })
+// }
+
+
+// function doPredict() {
+//   app.models.predict(Clarifai.appModel, image).then(function(response) {
+//       if(response.rawData.outputs[0].data.concepts) {
+//         var tag = response.outputs[0].data.concepts[0].name;
+//         var url = 'http://api.wolframalpha.com/v2/query?input='+tag+'%20nutrition%20facts&appid='+nutrientAPI;
+//         console.log(url)
+
+//         // getNutritionalInfo(url, function (result) {
+//         //   $('#concepts').html('<h3>'+ tag + '</h3>' + "<img src='"+result+"'>");
+//         // });
+//       }
+//     }, function(err) { console.log(err); }
+//   );
+// }
+
+
+
+$(getPreditedData)
 
 
 // $(function(){
@@ -76,13 +90,13 @@ plotly.plot(data, graphOptions, function (err, msg) {
 // })
 
 
-const zomatoapiKey = 'iaBarGgT3QLFcCGUavwLHM2ocQ398f8tojCVviYB'
-const zomatosearchURL = 'https://api.nps.gov/api/v1/parks'
+// const zomatoapiKey = 'iaBarGgT3QLFcCGUavwLHM2ocQ398f8tojCVviYB'
+// const zomatosearchURL = 'https://api.nps.gov/api/v1/parks'
 
-function formatQueryParams(params) {
-    const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-    return queryItems.join('&');
-}
+// function formatQueryParams(params) {
+//     const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+//     return queryItems.join('&');
+// }
 
 
 
